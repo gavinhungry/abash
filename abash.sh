2f2405189007872ad2c665d7fc7a86358e9d502b
Gavin Lloyd <gavinhungry@gmail.com>
2017-11-08 11:00:25 -0800

    Cleanup

--- abash.sh
+++ abash.sh
@@ -1,44 +1,42 @@
 #!/bin/bash
 #
-# Name: abash.sh
-# Auth: Gavin Lloyd <gavinhungry@gmail.com>
-# Desc: Small functions for bash shell scripts
+# abash: small functions for bash shell scripts
+# https://github.com/gavinhungry/abash
 #
 
-_ABASH=1
+export _ABASH=1
 
 usage() {
-  echo -e "\033[1musage\033[0m: $(basename $0) $@" >&2
+  echo -e "\033[1musage\033[0m: $(basename "$0") $*" >&2
   exit 1
 }
 
 pref() {
-  [ ${!1:-0} -eq 1 ] && return
+  [ "${!1:-0}" -eq 1 ] && return
 }
 
 arg() {
-  [ $# -ne 1 -a $# -ne 2 ] && return
+  [ $# -ne 1 ] && [ $# -ne 2 ] && return
 
-  local LONG=$(echo $1 | cut -d: -f1)
-  local SHORT=$(echo $1 | cut -sd: -f2)
+  local LONG SHORT
+  LONG=$(echo "$1" | cut -d: -f1)
+  SHORT=$(echo "$1" | cut -sd: -f2)
+
   local DEFAULT=${2:-}
-
-  local LONG_ARG="--${LONG}"
+  local LONG_ARG="--$LONG"
   local SHORT_ARG="-${SHORT:-${LONG:0:1}}"
 
-  [ ${#1} -eq 1 ] && COMP="-$1" || COMP="--$1"
-
   for (( I=0; I<${BASH_ARGC:-0}; I++ )); do
-    if [ "${BASH_ARGV[$I]}" == "${LONG_ARG}" -o \
-         "${BASH_ARGV[$I]}" == "${SHORT_ARG}" ]; then
-      echo ${BASH_ARGV[$I-1]}
+    if [ "${BASH_ARGV[$I]}" == "$LONG_ARG" ] ||
+       [ "${BASH_ARGV[$I]}" == "$SHORT_ARG" ]; then
+      echo "${BASH_ARGV[$I-1]}"
       return 0
       break
     fi
   done
 
-  if [ -n "${DEFAULT}" ]; then
-    echo ${DEFAULT}
+  if [ -n "$DEFAULT" ]; then
+    echo "$DEFAULT"
     return 0
   fi
 
@@ -46,53 +44,51 @@ arg() {
 }
 
 arge() {
-  arg $1 &> /dev/null
+  arg "$1" &> /dev/null
 }
 
 nfargs() {
-  for (( I=$BASH_ARGC-1; I>=0; I-- )); do
-    [[ ${BASH_ARGV[$I]} != -* ]] && echo ${BASH_ARGV[$I]}
+  for (( I=$((BASH_ARGC[0] - 1)); I>=0; I-- )); do
+    [[ "${BASH_ARGV[$I]}" != -* ]] && echo "${BASH_ARGV[$I]}"
   done
 }
 
 fnfarg() {
   NFARGS=($(nfargs))
-  echo ${NFARGS[0]}
+  echo "${NFARGS[0]}"
 }
 
 tmpdirp() {
-  local TMP="${TMPDIR:-/tmp}/$(basename $0)-$(whoami)/${1:-tmp}"
+  local TMP
+  TMP="${TMPDIR:-/tmp}/$(basename "$0")-$(whoami)/${1:-tmp}"
 
-  mkdir -p $TMP
-  echo $TMP
+  mkdir -p "$TMP"
+  echo "$TMP"
 }
 
 tmpdir() {
-  local TMP="${TMPDIR:-/tmp}/$(basename $0)-$(whoami)-$$/${1:-tmp}-${RANDOM}"
+  local TMP
+  TMP="${TMPDIR:-/tmp}/$(basename "$0")-$(whoami)-$$/${1:-tmp}-${RANDOM}"
 
-  mkdir -p $TMP
-  echo $TMP
+  mkdir -p "$TMP"
+  echo "$TMP"
 }
 
 tmpdirclean() {
-  rm -fr "${TMPDIR:-/tmp}/$(basename $0)-$(whoami)"{,-$$}
+  rm -fr "${TMPDIR:-/tmp}/$(basename "$0")-$(whoami)"{,-$$}
 }
 
 quietly() {
-  if arge verbose; then
-    eval "$@"
-  else
-    eval "$@" &> /dev/null
-  fi
+  (arge verbose && eval "$*") || eval "$*" &> /dev/null
 }
 
-print() {
+_print() {
   local ATTR=$1; shift
   local FG=$1; shift
   local BG=$1; shift
   local BANNER=$1; shift
 
-  echo -e "\e[${ATTR};${FG};${BG}m${BANNER}\e[0m$@"
+  echo -e "\e[${ATTR};${FG};${BG}m${BANNER}\e[0m$*"
 }
 
 msg() {
@@ -103,33 +99,33 @@ msg() {
   [ "$1" = "-i" ] && shift && FG=32 # green
   [ "$1" = "-w" ] && shift && FG=33 # yellow
 
-  print 1 $FG $BG "$(basename $0)" ": $@"
+  _print 1 $FG $BG "$(basename "$0")" ": $*"
 }
 
 inform() {
-  msg -i "$@ ..."
+  msg -i "$* ..."
 }
 
 err() {
-  msg -e "$@" >&2
+  msg -e "$*" >&2
 }
 
 warn() {
-  msg -w "$@" >&2
-  continue &> /dev/null
+  msg -w "$*" >&2
 }
 
 banner() {
-  print 1 37 44 "$@"
+  local BANNER=$1; shift
+  _print 1 37 44 "$BANNER" " $*"
 }
 
 bannerline() {
-  local ARGS=$@
+  local ARGS="$*"
   banner "${ARGS}$(printf '%*s' $((${COLUMNS:-$(tput cols)} - ${#ARGS})) '')"
 }
 
 die() {
-  err "$@"
+  err "$*"
   exit 1
 }
 
@@ -146,17 +142,18 @@ sigint() {
   trap _sigint SIGINT
 }
 
-is_int() {
+isint() {
   [ "$1" -eq "$1" ] &> /dev/null
 }
 
 pidpid() {
-  is_int $1 && echo $1 || pidof -s $1 2> /dev/null
+  (isint "$1" && echo "$1") || pidof -s "$1" 2> /dev/null
 }
 
 confirm() {
-  read -p "$1 [Y/n] " CONFIRM
+  local CONFIRM
+  read -rp "$* [Y/n] " CONFIRM
   echo
 
-  [ "$CONFIRM" == "Y" -o "$CONFIRM" == "Yes" -o -z "$CONFIRM" ] || exit 1
+  [ "${CONFIRM,,}" == "y" ] || [ "${CONFIRM,,}" == "yes" ] || [ -z "$CONFIRM" ] || exit 1
 }
